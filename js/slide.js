8d8c09d1830262bd662fedcda057181a539b9948
JMDo <k.dragonic@gmail.com>
2022-09-22 15:56:35 +0900

    슬라이드 es6로 변환중

--- js/slide.js
+++ js/slide.js
@@ -1,48 +1,34 @@
 $(function () {
 
     let startClientY = 0,
-        pixelOffset = 0,
-        touchStartY = 0,
-        touchEndY = 0 ;
+        touchStartY = 0;
 
-    let $slideArea = $('.slide-area');
-    let $slideBox = $('.slide-box');
-    let $slideContent = $('.slide-content');
+    let slideArea = document.getElementById('slide-area');
+    let slideBox = document.getElementById('slide-box');
 
-    $slideArea.on('mousedown touchstart' , slideStart);
-    $slideArea.on('mouseup touchend' , slideEnd);
-    $slideArea.on('mousemove touchmove' , slide);
+    // slideArea.on('mousedown touchstart' , slideStart);
+    // slideArea.on('mouseup touchend' , slideEnd);
+    console.log('체크::::::::::::' + slideArea + ':::::::::' + slideBox);
+    slideArea.addEventListener('mousedown touchstart',slideStart)
+    slideArea.addEventListener('mouseup touchend', slideEnd)
 
     function slideStart(event) {
         event.preventDefault();
         touchStartY = event.originalEvent.changedTouches[0].screenY;
         event = event.originalEvent.touches[0];
         startClientY = event.clientY;
-
-    }
-
-    function slide(event) {
-        event.preventDefault();
-        event = event.originalEvent.touches[0];
-        deltaSlide = event.clientY - startClientY;
-        pixelOffest = startPixelOffest + deltaSlide;
-        $slideBox.css('transfom', 'translateY(' + pixelOffset + 'px').removeClass();
-
+        slideBox.removeClass('fade');
+        document.querySelector('body').style.overflow = 'hidden';
+        console.log('테스트 시작');
     }
 
     function slideEnd(event){
         event.preventDefault();
         toychEndY = event.originalEvent.changedTouches[0].screenY;
-        if (touchEndY = touchStartY  > 20) {
-            $slideArea.fadeOut();
-            $slideContent.removeClass('on_slide');
-        }
-
+            slideBox.addClass('fade');
+            slideArea.removeClass('on_slide');
+            document.querySelector('body').style.overflow = '';
+            console.log('테스트 끝');
     }
-
-
-
-
-
 
 })
