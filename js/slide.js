49a7e36c08bd8dfcbe959b0bfc57489c5ce7b520
JMDo <k.dragonic@gmail.com>
2022-09-27 15:21:34 +0900

    gulp 세팅 및 스크립트 수정

--- js/slide.js
+++ js/slide.js
@@ -1,23 +1,21 @@
 
+
+
+
     let startClientY = 0,
         touchStartY = 0;
     let slideBox = document.getElementById('slide-box');
     let slideArea = document.getElementById('slide-area');
 
-    // slideArea.on('mousedown touchstart' , slideStart);
-    // slideArea.on('mouseup touchend' , slideEnd);
-    console.log('체크::::::::::::' + slideArea + ':::::::::' + slideBox);
     slideArea.addEventListener('touchstart',slideStart)
     slideArea.addEventListener('touchend', slideEnd)
 
     function slideStart(event) {
         event.preventDefault();
         touchStartY = event.changedTouches[0].screenY;
-        console.log('터치슬라이드' + touchStartY);
         event = event.touches[0];
         startClientY = event.clientY;
         document.querySelector('body').style.overflow = 'hidden';
-        console.log('테스트 시작');
     }
 
     function slideEnd(event){
@@ -25,5 +23,4 @@
         toychEndY = event.changedTouches[0].screenY;
             slideArea.classList.remove('on_slide');
             document.querySelector('body').style.overflow = '';
-            console.log('테스트 끝');
     }
