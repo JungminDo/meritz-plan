436bae7f106f7b532355feb91e098dd4b3c4fc6d
JMDo <k.dragonic@gmail.com>
2022-09-28 17:25:28 +0900

    스와이퍼추가

--- js/slide.js
+++ js/slide.js
@@ -1,26 +1,25 @@
+// pc 버전 일때
 
+// mo 버전 일때 반복 코드 줄이는 방법 연습 및 생각
 
+let startClientY = 0,
+    touchStartY = 0;
+let slideBox = document.getElementById("slide-box");
+let slideArea = document.getElementById("slide-area");
 
+slideArea.addEventListener("touchstart", slideStart);
+slideArea.addEventListener("touchend", slideEnd);
 
-    let startClientY = 0,
-        touchStartY = 0;
-    let slideBox = document.getElementById('slide-box');
-    let slideArea = document.getElementById('slide-area');
-
-    slideArea.addEventListener('touchstart',slideStart)
-    slideArea.addEventListener('touchend', slideEnd)
-
-    function slideStart(event) {
-        event.preventDefault();
-        touchStartY = event.changedTouches[0].screenY;
-        event = event.touches[0];
-        startClientY = event.clientY;
-        document.querySelector('body').style.overflow = 'hidden';
-    }
-
-    function slideEnd(event){
-        event.preventDefault();
-        toychEndY = event.changedTouches[0].screenY;
-            slideArea.classList.remove('on_slide');
-            document.querySelector('body').style.overflow = '';
-    }
+function slideStart(event) {
+    event.preventDefault();
+    touchStartY = event.changedTouches[0].screenY;
+    event = event.touches[0];
+    startClientY = event.clientY;
+    document.querySelector("body").style.overflow = "hidden";
+}
+function slideEnd(event) {
+    event.preventDefault();
+    toychEndY = event.changedTouches[0].screenY;
+    slideArea.classList.remove("on_slide");
+    document.querySelector("body").style.overflow = "";
+}
