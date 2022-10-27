0d3913b8fd9cb795f216d5fd7dad001fdc33cdc6
JMDo <k.dragonic@gmail.com>
2022-10-27 17:39:21 +0900

    color_flipper 추가

---
+++ js/quest01.js
@@ -0,0 +1,17 @@
+// quest01 스크립트 시 사용해야할 코드
+// arrays
+// document.getElementById()
+// document.querySelector()
+// addEventListener()
+// document.body.style.backgroundColor
+// Math.floor()
+// Math.random()
+// array.length
+
+
+// arrays 에 컬러 담기
+
+// 클릭시 실행 함수
+
+// 배열에 담긴 컬러값 랜덤으로 불러오기
+ 
